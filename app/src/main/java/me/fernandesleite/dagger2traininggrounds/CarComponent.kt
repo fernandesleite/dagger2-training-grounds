f28982384aaba4586fe26d35f2ff2ee1f8e223e6
Bruno Fernandes <brunofdsleite@gmail.com>
2021-04-30 20:20:34 -0300

    Field Injection

--- app/src/main/java/me/fernandesleite/dagger2traininggrounds/CarComponent.kt
+++ app/src/main/java/me/fernandesleite/dagger2traininggrounds/CarComponent.kt
@@ -7,5 +7,7 @@ import dagger.Component
 @Component
 interface CarComponent {
     fun getCar(): Car
+
+    fun inject(mainActivity: MainActivity)
 }
 
--- app/src/main/java/me/fernandesleite/dagger2traininggrounds/MainActivity.kt
+++ app/src/main/java/me/fernandesleite/dagger2traininggrounds/MainActivity.kt
@@ -2,15 +2,26 @@ package me.fernandesleite.dagger2traininggrounds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    lateinit var car: Car
+    /**
+     * Field Injection: when Dagger sets an @Inject annotated field on a class
+     * Use case: Injecting objects Dagger can't create, like mainActivity
+     */
+
+    @Inject lateinit var car: Car
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** Create Component **/
         val component: CarComponent = DaggerCarComponent.create()
-        car = component.getCar()
+
+        /** inject is a void method that take a parameter of a specific type **/
+        component.inject(this)
+
+        // car = component.getCar()
         car.drive()
     }
 }
